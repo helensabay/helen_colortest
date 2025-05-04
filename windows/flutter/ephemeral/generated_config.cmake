57b3141e9dca45a0f84c184b957dd39c92c700d1
helensabay14@gmail.com <helensabay14@gmail.com>
2025-05-04 08:16:40 +0800

    Update yellow color

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\helen\\OneDrive\\Pictures\\Documents\\flutter\\flutter_windows_3.10.6-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\helen\\hello_flutter" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\helen\\OneDrive\\Pictures\\Documents\\flutter\\flutter_windows_3.10.6-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\helen\\hello_flutter"
+  "FLUTTER_ROOT=C:\\Users\\helen\\OneDrive\\Pictures\\Documents\\flutter\\flutter_windows_3.10.6-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\helen\\hello_flutter\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\helen\\hello_flutter"
+  "FLUTTER_TARGET=C:\\Users\\helen\\hello_flutter\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jZGJlZGE3ODhhMjkzZmEyOTY2NWRjM2ZhM2Q2ZTYzYmQyMjFjYjBkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\helen\\hello_flutter\\.dart_tool\\package_config.json"
+)
