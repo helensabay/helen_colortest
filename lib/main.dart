3528090a7c9cb331a1d8436e0670012eb96801b6
helensabay <helensabay14@gmail.com>
2025-05-14 09:24:40 +0800

    Update color settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,25 @@
-ECHO is on.
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// TODO Create ChatApp widget
+// TODO Create LoginPage widget
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ), // ThemeData
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    ); // MaterialApp
+  }
+}
+
